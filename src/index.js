319405967ed59e3c059d169b471f17cb42aee76c
renderer-design <manuelajgampa@gmail.com>
2023-10-07 01:24:55 +0200

    troyan update 4

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ client.on('messageCreate', (message) => {
         arr = ['quien madruga se encuentra con todo cerrado😔🤙',
                     'para mi el locomotor es solo motor🥵😫',
                     'el tiempo sin ti es empo🙏🤟',
-                    'a veces me siento 🪑🤯'];
+                    'a veces las personas más frías solo necesitan un sueter😯🥶'];
         message.reply(arr[Math.floor(Math.random()*arr.length)]);
     }
 
