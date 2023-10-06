4cb67b3f24e0df99dd430b2a7e8e191f53f55ba7
Porrazo <69058122+porrazo@users.noreply.github.com>
2023-10-07 00:32:48 +0200

    troyano update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 const { Client, IntentsBitField } = require('discord.js');
-
+function choose(){ // devuelve, de unos argumentos, uno al azar. como el random, pero elige uno de los argumentos
+	var index=floor(Math.random()*(arguments.length-1));
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -27,6 +28,15 @@ client.on('messageCreate', (message) => {
     if(message.content.includes('sale evento')){
         message.reply('el de mis huevos al viento');
     }
+
+    if(message.content === ('!frasejoker')){
+        const arr = ['quien madruga se encuentra con todo cerrado😔🤙',
+                    'para mi el locomotor es solo motor🥵😫',
+                    'el tiempo sin ti es empo🙏🤟'];
+        message.reply(arr[choose(0,1,2)]);
+    }
+    
+    
 });
 
 client.login(process.env.DISCORD_TOKEN);
