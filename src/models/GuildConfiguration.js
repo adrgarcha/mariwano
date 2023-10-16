c3e93a296ba314e826a06723f1ed8f954a8b3926
Adrián García Chavero <adrgarcha@alum.us.es>
2023-10-17 00:55:00 +0200

    Add report system

--- src/models/GuildConfiguration.js
+++ src/models/GuildConfiguration.js
@@ -1,14 +1,18 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const guildConfigurationSchema = new Schema({
-    guildId: {
-        type: String,
-        required: true,
-    },
-    suggestionChannelIds: {
-        type: [String],
-        default: []
-    },
+  guildId: {
+    type: String,
+    required: true,
+  },
+  suggestionChannelIds: {
+    type: [String],
+    default: [],
+  },
+  reportChannelIds: {
+    type: [String],
+    default: [],
+  },
 });
 
-module.exports = model('GuildConfiguration', guildConfigurationSchema);
+module.exports = model("GuildConfiguration", guildConfigurationSchema);
