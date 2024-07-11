f34975a41bedb32c34864ce9a3d7a9ae230cb9a6
adrgarcha <adrgarcha@alum.us.es>
2024-07-11 18:09:02 +0200

    register commands always

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,7 @@ import path from 'path';
             client,
             commandsPath: path.join(__dirname, 'commands'),
             eventsPath: path.join(__dirname, 'events'),
+            bulkRegister: true,
          });
 
          client.login(process.env.DISCORD_TOKEN); // Despliegue
