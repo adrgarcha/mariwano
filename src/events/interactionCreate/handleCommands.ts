837876440fab30d63c8c1965293c93a711864e9e
renderer-design <manuelajgampa@gmail.com>
2024-07-14 21:20:52 +0200

    command registering hand-made

---
+++ src/events/interactionCreate/handleCommands.ts
@@ -0,0 +1,30 @@
+import { Interaction } from "discord.js";
+
+export default async function (interaction: Interaction) {
+  if (!interaction.isChatInputCommand()) return;
+  
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.run({ interaction });
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,44 @@
-import { CommandKit } from 'commandkit';
-import { Player } from 'discord-player';
-import { Client, GatewayIntentBits } from 'discord.js';
-import 'dotenv/config';
-import mongoose from 'mongoose';
-import path from 'path';
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+import { Player } from "discord-player";
+import { ChatInputCommandInteraction, Partials } from "discord.js";
+
+import "dotenv/config";
+import mongoose from "mongoose";
+import { commandHandler } from "./lib/commandhandler";
+import { deploycommands } from "./lib/deploycommands";
+import { eventHandler } from "./lib/eventhandler";
 
 (async () => {
-   const client = new Client({
-      intents: [
-         GatewayIntentBits.Guilds,
-         GatewayIntentBits.GuildMembers,
-         GatewayIntentBits.GuildMessages,
-         GatewayIntentBits.GuildPresences,
-         GatewayIntentBits.GuildVoiceStates,
-         GatewayIntentBits.MessageContent,
-      ],
-   });
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildPresences,
+      GatewayIntentBits.GuildVoiceStates,
+      GatewayIntentBits.MessageContent,
+    ],
+    partials: [Partials.Channel],
+  });
+  client.commands = new Collection();
+  const player = new Player(client);
+  await player.extractors.loadDefault();
 
-   const player = new Player(client);
-   await player.extractors.loadDefault();
+  player.events.on("playerStart", (queue, track) => {
+    queue.metadata.channel.send(`Reproduciendo **${track.title}**.`);
+  });
 
-   player.events.on('playerStart', (queue, track) => {
-      queue.metadata.channel.send(`Reproduciendo **${track.title}**.`);
-   });
-
-   (async () => {
-      try {
-         await mongoose.connect(process.env.MONGODB_URI!);
-         console.log('Conectado a la base de datos.');
-
-         new CommandKit({
-            client,
-            commandsPath: path.join(__dirname, 'commands'),
-            eventsPath: path.join(__dirname, 'events'),
-         });
-
-         // client.login(process.env.DISCORD_TOKEN); // Despliegue
-         client.login(process.env.DISCORD_TEST_TOKEN); // Testing
-      } catch (error) {
-         console.error(`Hubo un error al conectar con la base de datos: ${error}`);
-      }
-   })();
+  (async () => {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI!);
+      console.log("Conectado a la base de datos.");
+      commandHandler(client);
+      deploycommands(client);
+      eventHandler(client);
+      // client.login(process.env.DISCORD_TOKEN); // Despliegue
+      client.login(process.env.DISCORD_TEST_TOKEN); // Testing
+    } catch (error) {
+      console.error(`Hubo un error al conectar con la base de datos: ${error}`);
+    }
+  })();
 })();
