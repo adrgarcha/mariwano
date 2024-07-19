2385e32d418fe56ed93d8c66169b6922946fa194
renderer-design <manuelajgampa@gmail.com>
2024-07-19 20:36:49 +0200

    User.ts updated schemas only accepts integers on economy commands

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,50 +1,50 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
 const userSchema = new Schema({
-   userId: {
-      type: String,
-      required: true,
-   },
-   guildId: {
-      type: String,
-      required: true,
-   },
-   balance: {
-      type: Number,
-      default: 0,
-   },
-   lastDaily: {
-      type: Date,
-      default: new Date(),
-   },
-   lastWordle: {
-      type: Date,
-      default: new Date(),
-   },
-   kahootLimit: {
-      type: Number,
-      default: 5,
-   },
-   lastKahoot: {
-      type: Date,
-      default: new Date(),
-   },
-   customRoleId: {
-      type: String,
-      default: '',
-   },
-   invested: {
-      type: Number,
-      default: 0,
-   },
-   investBankFactor: {
-      type: Number,
-      default: 0,
-   },
-   investFactor: {
-      type: Number,
-      default: 0,
-   },
+  userId: {
+    type: String,
+    required: true,
+  },
+  guildId: {
+    type: String,
+    required: true,
+  },
+  balance: {
+    type: BigInt,
+    default: 0,
+  },
+  lastDaily: {
+    type: Date,
+    default: new Date(),
+  },
+  lastWordle: {
+    type: Date,
+    default: new Date(),
+  },
+  kahootLimit: {
+    type: Number,
+    default: 5,
+  },
+  lastKahoot: {
+    type: Date,
+    default: new Date(),
+  },
+  customRoleId: {
+    type: String,
+    default: "",
+  },
+  invested: {
+    type: BigInt,
+    default: 0,
+  },
+  investBankFactor: {
+    type: BigInt,
+    default: 0,
+  },
+  investFactor: {
+    type: BigInt,
+    default: 0,
+  },
 });
 
-export const User = model('User', userSchema);
+export const User = model("User", userSchema);
