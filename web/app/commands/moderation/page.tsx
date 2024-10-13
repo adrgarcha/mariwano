1e0e2c79527de30a66639058eb6838cf52f38e73
adrgarcha <adrgarcha@alum.us.es>
2024-10-13 22:56:59 +0200

    feat: add moderation page

---
+++ web/app/commands/moderation/page.tsx
@@ -0,0 +1,20 @@
+import Command from '@/components/command';
+
+export default function Moderation() {
+   return (
+      <>
+         <Command title="ban" params={['target-user', 'reason']}>
+            Banea a el usuario introducido en target-user con la opción de dar una razón en la casilla de reason. El usuario no podrá volver a entrar
+            hasta que un administrador le quite el baneo.
+         </Command>
+         <Command title="kick" params={['target-user', 'reason']}>
+            Expulsa del servidor al usuario introducido en target-user con la opción de dar una razón con el parámetro reason. El usuario podrá ser
+            invitado de vuelta al servidor.
+         </Command>
+         <Command title="timeout" params={['target-user', 'duration', 'reason']}>
+            Banea a un usuario por un determinado tiempo puesto en duration. En dicho parámetro se debe introducir la cantidad de tiempo junto la
+            primera letra de la unidad de tiempo, por ejemplo 15m resultaría en un baneo de 15 minutos.
+         </Command>
+      </>
+   );
+}
