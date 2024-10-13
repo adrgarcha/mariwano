5192809a5d109768a378334da988cf588f8977f5
adrgarcha <adrgarcha@alum.us.es>
2024-10-13 22:42:42 +0200

    fix: join string params

--- web/components/command.tsx
+++ web/components/command.tsx
@@ -8,7 +8,7 @@ export default function Command({ title, params, children }: CommandProps) {
    return (
       <section className="flex flex-col gap-y-1">
          <h3 className="font-bold text-2xl">/{title}</h3>
-         <h5 className="font-semibold">Parámetros: {params ? params : 'Ninguno'}</h5>
+         <h5 className="font-semibold">Parámetros: {params ? params.join(', ') : 'Ninguno'}</h5>
          <p className="font-medium text-white/80">{children}</p>
       </section>
    );
