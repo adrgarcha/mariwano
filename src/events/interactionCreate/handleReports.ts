b9b9b7224755b89606bf32fb059f5725c2bd5ee1
adrgarcha <adrgarcha@alum.us.es>
2026-01-06 18:06:13 +0100

    fix: resolve issue with message ids in reports and improve code

--- src/events/interactionCreate/handleReports.ts
+++ src/events/interactionCreate/handleReports.ts
@@ -1,4 +1,4 @@
-import { EmbedBuilder, Interaction } from 'discord.js';
+import { EmbedBuilder, Interaction, MessageFlags } from 'discord.js';
 import { Report } from '../../models/Report';
 
 export default async function (interaction: Interaction) {
@@ -11,30 +11,30 @@ export default async function (interaction: Interaction) {
 
       if (type !== 'report') return;
 
-      await interaction.deferReply({ ephemeral: true });
+      if (!interaction.memberPermissions?.has('Administrator')) {
+         await interaction.editReply('No tienes permisos para marcar como solucionado un informe.');
+         return;
+      }
+
+      await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 
       const targetReport = await Report.findOne({ reportId });
-      const targetReportId = targetReport?.reportId;
 
-      if (!targetReportId) {
+      if (!targetReport) {
          await interaction.editReply('No se ha encontrado el informe.');
          return;
       }
 
-      const targetMessage = await interaction.channel?.messages.fetch(targetReportId);
+      const targetMessageId = targetReport.messageId;
+      const targetMessage = await interaction.channel?.messages.fetch(targetMessageId);
       const targetMessageEmbed = targetMessage?.embeds[0];
 
+      if (!targetMessageEmbed) {
+         await interaction.editReply('Ha ocurrido un error al obtener el mensaje del informe.');
+         return;
+      }
+
       if (action === 'solved') {
-         if (!interaction.memberPermissions?.has('Administrator')) {
-            await interaction.editReply('No tienes permisos para marcar como solucionado un informe.');
-            return;
-         }
-
-         if (!targetMessageEmbed) {
-            await interaction.editReply('Ha ocurrido un error al obtener el mensaje del informe.');
-            return;
-         }
-
          targetMessageEmbed.fields[2].value = '✅ Solucionado';
          const updatedEmbed = EmbedBuilder.from(targetMessageEmbed).setColor(0x84e660);
 
@@ -42,24 +42,11 @@ export default async function (interaction: Interaction) {
 
          interaction.editReply('Error solucionado. Muchas gracias por su informe.');
 
-         targetMessage?.edit({
-            embeds: [updatedEmbed],
-            components: [],
-         });
+         targetMessage?.edit({ embeds: [updatedEmbed], components: [] });
          return;
       }
 
       if (action === 'fake') {
-         if (!interaction.memberPermissions?.has('Administrator')) {
-            await interaction.editReply('No tienes permisos para marcar como falso un informe.');
-            return;
-         }
-
-         if (!targetMessageEmbed) {
-            await interaction.editReply('Ha ocurrido un error al obtener el mensaje del informe.');
-            return;
-         }
-
          targetMessageEmbed.fields[2].value = '❕ Falso';
          const updatedEmbed = EmbedBuilder.from(targetMessageEmbed).setColor(0xffffff);
 
@@ -67,10 +54,7 @@ export default async function (interaction: Interaction) {
 
          interaction.editReply('Informe falso. Se tomaran medidas con el usuario conveniente por divulgar información falsa.');
 
-         targetMessage?.edit({
-            embeds: [updatedEmbed],
-            components: [],
-         });
+         targetMessage?.edit({ embeds: [updatedEmbed], components: [] });
          return;
       }
    } catch (error) {
